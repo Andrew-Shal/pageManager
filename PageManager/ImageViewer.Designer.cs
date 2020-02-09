7f731cbc85b4145dca43b1f6f4cf4d855389aa65
Andrew-Shal <shalandrew97@gmail.com>
2020-02-09 12:48:19 -0600

    first commit

---
+++ PageManager/ImageViewer.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace PageManager
+{
+    partial class ImageViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectImageBtn = new System.Windows.Forms.Button();
+            this.imagePathField = new System.Windows.Forms.TextBox();
+            this.openImageDialog = new System.Windows.Forms.OpenFileDialog();
+            this.savePageBtn = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.ImageViewModule = new PageManager.ImagePanel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectImageBtn
+            // 
+            this.selectImageBtn.Location = new System.Drawing.Point(166, 41);
+            this.selectImageBtn.Name = "selectImageBtn";
+            this.selectImageBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectImageBtn.TabIndex = 0;
+            this.selectImageBtn.Text = "Open Image";
+            this.selectImageBtn.UseVisualStyleBackColor = true;
+            this.selectImageBtn.Click += new System.EventHandler(this.selectImageBtn_Click);
+            // 
+            // imagePathField
+            // 
+            this.imagePathField.Location = new System.Drawing.Point(12, 44);
+            this.imagePathField.Name = "imagePathField";
+            this.imagePathField.Size = new System.Drawing.Size(148, 20);
+            this.imagePathField.TabIndex = 1;
+            // 
+            // openImageDialog
+            // 
+            this.openImageDialog.FileName = "openFileDialog1";
+            // 
+            // savePageBtn
+            // 
+            this.savePageBtn.Location = new System.Drawing.Point(566, 402);
+            this.savePageBtn.Name = "savePageBtn";
+            this.savePageBtn.Size = new System.Drawing.Size(75, 23);
+            this.savePageBtn.TabIndex = 2;
+            this.savePageBtn.Text = "Save Page";
+            this.savePageBtn.UseVisualStyleBackColor = true;
+            this.savePageBtn.Click += new System.EventHandler(this.savePageBtn_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(566, 351);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(172, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // ImageViewModule
+            // 
+            this.ImageViewModule.CanvasSize = new System.Drawing.Size(60, 40);
+            this.ImageViewModule.Image = null;
+            this.ImageViewModule.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+            this.ImageViewModule.Location = new System.Drawing.Point(12, 84);
+            this.ImageViewModule.Name = "ImageViewModule";
+            this.ImageViewModule.Size = new System.Drawing.Size(726, 260);
+            this.ImageViewModule.TabIndex = 3;
+            this.ImageViewModule.Zoom = 1F;
+            // 
+            // ImageViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.ImageViewModule);
+            this.Controls.Add(this.savePageBtn);
+            this.Controls.Add(this.imagePathField);
+            this.Controls.Add(this.selectImageBtn);
+            this.Name = "ImageViewer";
+            this.Text = "ImageViewer";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectImageBtn;
+        private System.Windows.Forms.TextBox imagePathField;
+        private System.Windows.Forms.OpenFileDialog openImageDialog;
+        private System.Windows.Forms.Button savePageBtn;
+        private ImagePanel ImageViewModule;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ PageManager/ImageViewer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace PageManager
+{
+    public partial class ImageViewer : Form
+    {
+        string imagePath = null;
+
+        public ImageViewer()
+        {
+            InitializeComponent();
+
+            // set open image dialog to restrict files
+            openImageDialog.Filter = "Image files (*.jpg,*.jpeg,*.jpe,*.jfif,*.png,*.tiff)|*.jpg;*.jpeg;*.jpe;*.jfif;*.png;*.tiff";
+            trackBar1.SetRange(10, 50);
+        }
+
+        private void selectImageBtn_Click(object sender, EventArgs e)
+        {
+            if (openImageDialog.ShowDialog() == DialogResult.OK) {
+                imagePath = openImageDialog.FileName;    // image path
+
+                imagePathField.Text = imagePath;
+
+                Bitmap b = new Bitmap(imagePath);
+                ImageViewModule.CanvasSize = b.Size;
+                ImageViewModule.Image = b;
+                ImageViewModule.Zoom = trackBar1.Value * 0.02f;
+            }
+            
+        }
+
+        private void savePageBtn_Click(object sender, EventArgs e)
+        {
+            // testing variables ~ will be dynamic in main application
+            const string LedgerID = "HELLO";    // is folder name
+            const int PageNumber = 1;   // is image name
+            const string DumpFolder = "dump";   // name of dump folder for exisiting images
+
+            string sourceFile = imagePath;  
+            string imageExtention = Path.GetExtension(imagePath);   // get image extension type
+            string destinationFolder = @"C:\Users\shala\Desktop\" + LedgerID + @"\";   // 
+            string destinationFile = destinationFolder + PageNumber + imageExtention;  // hard coded path
+            try {
+
+                Directory.CreateDirectory(destinationFolder);   // create ledger folder
+                
+                if (File.Exists(destinationFile)) {
+                    // create a dump folder 
+                    // move existing image to dump 
+                    // proceed to paste new image
+
+                    string dumpFolder = destinationFolder + DumpFolder + @"\";   // dump folder for current ledger
+                    string dumpFile = dumpFolder + PageNumber + imageExtention;   // dump file location for current page in ledger
+                    
+                    Directory.CreateDirectory(dumpFolder);   // create dump folder
+
+                    if (File.Exists(dumpFile))
+                    {
+                        // an exisiting dump image already exists for current ledger
+                        // count number of images in dump folder
+                        // create name of dump imagename_(numimage+1)
+
+                        // count files in dump directory
+                        int fCount = Directory.GetFiles(dumpFolder, "*", SearchOption.TopDirectoryOnly).Length;
+                        string newDumpFileName = PageNumber + "_" + fCount; // new dump image name
+                        string newDumpFile = dumpFolder + newDumpFileName + imageExtention; // create new dump path with renamed dump image
+                        File.Move(destinationFile,newDumpFile);
+                    }
+                    else { 
+                        // no dump image(s) for current page in currrent ledger
+                        File.Move(destinationFile,dumpFile);   // moves the exisiting file
+                    }
+                }
+
+                File.Copy(sourceFile,destinationFile,false);   // copy image to
+
+            }catch(InvalidOperationException iox){
+                MessageBox.Show(iox.Message);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            ImageViewModule.Zoom = trackBar1.Value * 0.02f;
+        }
+    }
+}
